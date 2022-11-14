05d9ff70977235e0fba61c6f9a485eb4a47c4355
柳生真輝 <nousidayo@gmail.com>
2022-11-14 16:55:23 +0900

    ユーザーの生成処理を追加

--- modules/routers/routes.go
+++ modules/routers/routes.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"howToUseMod/modules/users"
 	"howToUseMod/modules/users/UserController"
 	"net/http"
 
@@ -14,7 +15,17 @@ func Main() {
 	})
 	userRoutes := r.Group("/users")
 	userRoutes.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, UserController.Show(1).Name)
+		user := UserController.Show(1)
+		c.String(http.StatusOK, user.Name)
+	})
+	userRoutes.POST("/create", func(c *gin.Context) {
+		var user users.User
+		// user.Name = c.PostForm("name")
+		// user.Email = c.PostForm("email")
+		user.Name = "これが名前"
+		user.Email = "test@test.com"
+		user.Create()
+		c.String(http.StatusOK, "ユーザーの作成に成功しました")
 	})
 	r.Run()
 }
